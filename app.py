038d3efd5c9092401e1e677baa22c071a10069cc
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2023-03-08 13:15:09 -0300

    creating a blank window

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import pygame
+from sys import exit
 
-pygame.init()
+pygame.init() # initialize pygame
+screen = pygame.display.set_mode((800, 400)) # create the screen
+pygame.display.set_caption('Runner')
+
+while True: # runs forever
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # QUIT, ACTIVEEVENT, KEYDOWN, KEYUP, MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN, JOYAXISMOTION, JOYBALLMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN, VIDEORESIZE, VIDEOEXPOSE, USEREVENT
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update() # updates the 'screen' display surface
