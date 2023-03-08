5a4d6aeba10a5413e6e9985bf5b53125763f0357
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2023-03-08 13:40:29 -0300

    clock + parte visual/sons

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from sys import exit
 pygame.init() # initialize pygame
 screen = pygame.display.set_mode((800, 400)) # create the screen
 pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200)) # width, height
+test_surface.fill('Red')
 
 while True: # runs forever
     for event in pygame.event.get():
@@ -11,6 +15,10 @@ while True: # runs forever
             # QUIT, ACTIVEEVENT, KEYDOWN, KEYUP, MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN, JOYAXISMOTION, JOYBALLMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN, VIDEORESIZE, VIDEOEXPOSE, USEREVENT
             pygame.quit()
             exit()
+
     # draw all our elements
+    screen.blit(test_surface, (200, 100))
+    
     # update everything
     pygame.display.update() # updates the 'screen' display surface
+    clock.tick(60)
