3775e419643e5edf38b3f4462ed7c00ad0f93c89
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2023-03-08 15:17:02 -0300

    rectangles

--- app.py
+++ app.py
@@ -14,7 +14,12 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black') # text, AA, color
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 800
+# snail_x_pos = 800
+snail_rectangle = snail_surface.get_rect(bottomright = (800, 300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+# player_rectangle = pygame.Rect() # left, top, width, height
+player_rectangle = player_surface.get_rect(midbottom = (50, 300)) # topleft = (x,y) / midleft, midbottom, topleft, etc
 
 while True: # runs forever
     for event in pygame.event.get():
@@ -29,9 +34,12 @@ while True: # runs forever
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (340, 50))
 
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 265))
+    # snail_x_pos -= 4
+    # if snail_x_pos < -100: snail_x_pos = 800
+    snail_rectangle.x -= 3
+    if snail_rectangle.right <= 0: snail_rectangle.left = 800
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
 
     # update everything
     pygame.display.update() # updates the 'screen' display surface
