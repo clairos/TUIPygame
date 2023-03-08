bcdce68cbe11037189ea939fcd3567189b014c59
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2023-03-08 14:08:10 -0300

    basic animation

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50) # font type, font size
 
 # test_surface = pygame.Surface((100, 200)) # width, height
 # test_surface.fill('Red')
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black') # text, AA, color
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 800
 
 while True: # runs forever
     for event in pygame.event.get():
@@ -25,6 +28,10 @@ while True: # runs forever
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (340, 50))
+
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 265))
 
     # update everything
     pygame.display.update() # updates the 'screen' display surface
